7568f53008092dd36779037d4426439d23204b81
hreinberger <40197225+hreinberger@users.noreply.github.com>
2024-05-14 12:31:38 +0200

    add github token and cloudflare tunnel to setup

--- container/apache/entrypoint.d/040-mondu.sh
+++ container/apache/entrypoint.d/040-mondu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+composer config github-oauth.github.com ${GITHUB_TOKEN}
 composer config repositories.mondu/bnpl-checkout-oxid vcs https://github.com/mondu-ai/bnpl-checkout-oxid.git
 composer require mondu/bnpl-checkout-oxid
